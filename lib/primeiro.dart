4f58cbd8f3f9fb5af0509d649f4d627b8a39542b
FranciscoGabrielCavalcanteCandido <fg.14.cavalcandido@gmail.com>
2022-04-05 20:41:46 -0300

    segunda alteraçao

--- lib/primeiro.dart
+++ lib/primeiro.dart
@@ -1,3 +1,10 @@
 void minha_funcao() {
-  print("Hello World!");
+  //tipo nome = atribuição
+  String nomeProduto = "Coca cola";
+  double precoProduto = 10;
+  int quantidadeProduto = 1;
+  String? descricao;
+  //para uma variavel ser nula é necessario o ponto de ?
+  print(
+      "O nome do produto é $nomeProduto, o preço está $precoProduto, e vamos comprar $quantidadeProduto");
 }
