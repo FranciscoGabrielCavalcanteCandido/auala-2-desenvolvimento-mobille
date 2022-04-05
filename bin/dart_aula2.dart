1d1d89d1b67b986ce08dfd18ae085a2a0b2802aa
FranciscoGabrielCavalcanteCandido <fg.14.cavalcandido@gmail.com>
2022-04-05 20:29:55 -0300

    primeira alteração

---
+++ bin/dart_aula2.dart
@@ -0,0 +1,7 @@
+import 'package:dart_aula2/dart_aula2.dart' as dart_aula2;
+import 'package:dart_aula2/primeiro.dart' as primeiro;
+
+void main(List<String> arguments) {
+  primeiro.minha_funcao();
+  //print('Hello world: ${primeiro.dart.calculate()}!');
+}
